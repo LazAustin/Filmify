df725f5f9db3f490c7fea24e34b92bb1c8ed485d
Laz Austin <lazaustin94@gmail.com>
2022-07-28 21:27:22 -0500

    deploying to heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv').config();
+// const colors = require('colors');
+// const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const cors = require('cors');
